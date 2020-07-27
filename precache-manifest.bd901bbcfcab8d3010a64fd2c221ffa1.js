1c07794b4576bebde52ce6da6fe608d5ebfc674f
cbrag11 <clintonbraganza11@gmail.com>
2020-07-26 23:39:02 -0700

    Updates

---
+++ precache-manifest.bd901bbcfcab8d3010a64fd2c221ffa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00279d67da9dbe8d528951fae2775686",
+    "url": "/brag-portfolio/index.html"
+  },
+  {
+    "revision": "b614fdc7cb3682679dc1",
+    "url": "/brag-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "232130b72ed232db356d",
+    "url": "/brag-portfolio/static/js/2.73b292a3.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/brag-portfolio/static/js/2.73b292a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b614fdc7cb3682679dc1",
+    "url": "/brag-portfolio/static/js/main.289c10b0.chunk.js"
+  },
+  {
+    "revision": "35cc159cae12fbf77a35",
+    "url": "/brag-portfolio/static/js/runtime-main.cd7998b5.js"
+  }
+]);
